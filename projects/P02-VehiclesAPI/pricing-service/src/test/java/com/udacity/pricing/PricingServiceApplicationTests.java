4f21a33a4835be1474408f8438a2ae98315249ca
Abhiram Iyer <abhirami@amazon.com>
2021-01-07 16:25:32 +0530

    Converted the original pricing-service module into microservice
    I had created a new microservice named vehicle-pricing-service to replace pricing-service. The rubric requires me to make changes to the original pricing-service module itself.

--- projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,41 @@
 package com.udacity.pricing;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.udacity.pricing.entity.Price;
+import org.apache.catalina.LifecycleState;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class PricingServiceApplicationTests {
+import java.util.List;
 
-	@Test
-	public void contextLoads() {
-	}
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PricingServiceApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void getAllPrices() {
+        ResponseEntity<Object> response = testRestTemplate.getForEntity("http://localhost:" + port + "/prices/", Object.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    void getPriceOfVehicleId1() {
+        ResponseEntity<Price> response = testRestTemplate.getForEntity("http://localhost:" + port + "/prices/1", Price.class);
+        Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
 
 }
--- projects/P02-VehiclesAPI/vehicle-pricing-service/src/main/java/com/abhiram/microservices/vehiclepricingservice/repository/PriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.abhiram.microservices.vehiclepricingservice.repository;
-
-import com.abhiram.microservices.vehiclepricingservice.entity.Price;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PriceRepository extends CrudRepository<Price, Long> {
-}
